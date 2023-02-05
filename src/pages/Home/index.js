9a9aa3ba229b46ba029533846313355ea6c4a8b7
Felipe Lopes <felipe.presidencia@gmail.com>
2023-02-05 11:51:05 -0300

    C1 - Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,59 @@
 /**
  * Home
  */
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { View, Text, SafeAreaView, FlatList, TouchableOpacity } from "react-native";
 import { styles } from "./styles";
+import { AntDesign } from '@expo/vector-icons';
 
-export default function Home(){
-  return(
-    <View style={styles.container}>
-      <Text>Home</Text>
-    </View>
+export default function Home() {
+
+  const [products, setProducts] = useState([
+    {
+      id: '1',
+      name: 'Coca cola',
+      price: 9.90
+    },
+    {
+      id: '2',
+      name: 'Chocolate',
+      price: 19.90
+    },
+    {
+      id: '3',
+      name: 'Queijo 500g',
+      price: 10.90
+    },
+    {
+      id: '4',
+      name: 'Batata frita',
+      price: 8.90
+    },
+    {
+      id: '5',
+      name: 'Guarana',
+      price: 5.90
+    },
+    {
+      id: '6',
+      name: 'Bolo',
+      price: 7.90
+    },
+  ])
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.cartContent}>
+        <Text style={styles.title}>Lista de produtos</Text>
+
+        <TouchableOpacity style={styles.cardButton}>
+          <View style={styles.dot}>
+            <Text style={styles.dotText}>3</Text>
+          </View>
+
+          <AntDesign name="shoppingcart" size={30} color="black" />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   )
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -3,8 +3,37 @@ import { StyleSheet } from "react-native"
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#E2E2E2',
+    backgroundColor: '#FAFAFA',
+    paddingEnd: 14,
+    paddingStart: 14,
+  },
+  cartContent: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'center',
+    marginTop: 24,
+    marginBottom: 24,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  cardButton: {
+
+  },
+  dot: {
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'red',
+    width: 20,
+    height: 20,
+    borderRadius: 12,
+    position: 'absolute',
+    zIndex: 99,
+    bottom: -2,
+    left: -4
+  },
+  dotText: {
+    fontSize: 12,
   },
 })
